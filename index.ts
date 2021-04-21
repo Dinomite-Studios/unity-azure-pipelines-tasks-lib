45a3a61861b2fb39966a98a19f0b0c7bac01ad7e
Dino Fejzagić <dino.f@live.de>
2021-04-21 22:25:20 +0200

    Add changelog file and update index.ts

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from './lib/unity-log-streamer';
 export * from './lib/unity-tool-runner';
 export * from './lib/unity-path-tools';
 export * from './lib/unity-version-tools';
+export * from './lib/utilities';
+export * from './lib/models';
