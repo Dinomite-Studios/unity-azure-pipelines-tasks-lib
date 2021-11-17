e220ac5c9974c6e2b5d842653d9ebc1181e46c16
Dino Fejzagić <dino.f@live.de>
2021-11-17 08:18:57 +0100

    Update dependencies

--- lib/unity-path-tools.ts
+++ lib/unity-path-tools.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Dinomite Studios. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 import { Utilities } from './utilities';
 import { OS, UnityVersionInfo } from './models';
 import path = require('path');
