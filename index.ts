426f4e82dd5d470eb110522f6d9d09e5d78ecc6c
Dino Fejzagić <dino.f@live.de>
2022-03-07 13:50:04 +0100

    Update dependencies (#3) (#4)
    * Update dependencies
    
    * Update fs-extra
    
    * Update changelog
    
    * Update dependencies

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Dinomite Studios. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 export * from './lib/unity-log-streamer';
 export * from './lib/unity-tool-runner';
 export * from './lib/unity-path-tools';
--- lib/utilities.ts
+++ lib/utilities.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Dinomite Studios. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 import { OS } from "./models";
 import sanitize from 'sanitize-filename';
 
