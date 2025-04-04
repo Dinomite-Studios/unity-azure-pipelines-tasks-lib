14d51bba64f84fe95dbf96c623118174149338da
Dino Fejzagić <dino.f@live.de>
2025-04-04 20:37:04 +0200

    Add definition and testing resource

---
+++ lib/unity-bundle-version-tools.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Dinomite. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+/**
+ * A collection of static functions to manipulate the bundle versrion of a Unity project.
+ */
+export class UnityVersionTools {
+
+    private static readonly unityProjectSettingsFolder = 'ProjectSettings';
+    private static readonly unityProjectSettingsFile = 'ProjectSettings.asset';
+
+    
+}
